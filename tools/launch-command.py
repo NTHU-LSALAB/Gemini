e7e7f4821d0c5e6300c49ebc866718a6ac2932c2
jim90247 <jim90247@gmail.com>
2020-05-23 01:16:55 +0800

    Update helper python scripts
    - Remove launch-backend.py, since launching backend can be done by
      simply running "gem-schd" (with some arguments if needed)
    
    - Update yapf formatter setting: use default line width limit

--- tools/launch-command.py
+++ tools/launch-command.py
@@ -26,9 +26,9 @@ from pathlib import Path
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--name', help='pod name', required=True)
-    parser.add_argument('--port', help='pod manager port', required=True)
-    parser.add_argument('--ip', help='pod manager ip', default='127.0.0.1')
+    parser.add_argument('--name', help='client group name', required=True)
+    parser.add_argument('--port', help='backend port', required=True)
+    parser.add_argument('--ip', help='backend ip', default='127.0.0.1')
     parser.add_argument('--timeout', type=int, help='seconds to run')
     parser.add_argument('command', nargs='+')
     args = parser.parse_args()
@@ -36,13 +36,19 @@ def main():
     os.setpgrp()
 
     client_env = os.environ.copy()
-    client_env['POD_MANAGER_IP'] = args.ip
-    client_env['POD_MANAGER_PORT'] = args.port
-    client_env['POD_NAME'] = args.name
-    client_env['LD_PRELOAD'] = "{}/gemini/lib/libgemhook.so.1".format(Path.home())
+    client_env['GEMINI_BACKEND_IP'] = args.ip
+    client_env['GEMINI_BACKEND_PORT'] = args.port
+    client_env['GEMINI_GROUP_NAME'] = args.name
+    client_env['LD_PRELOAD'] = "{}/gemini/lib/libgemhook.so.1".format(
+        Path.home()
+    )
 
     proc = sp.Popen(
-        args.command, env=client_env, start_new_session=True, universal_newlines=True, bufsize=1
+        args.command,
+        env=client_env,
+        start_new_session=True,
+        universal_newlines=True,
+        bufsize=1
     )
 
     print("[launcher] run: {}".format(args.command))
